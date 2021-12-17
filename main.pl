e06bccf4f576154cf9ca739e97b25cb70395b5a6
John <john.serrano@usach.cl>
2021-12-16 22:49:12 -0300

    Se crea el predicado paradigmaDocsRegister
    Se crea el primer predicado funcional, paradigmaDocsRegister, que permite verificar y registrar a un user en la plataforma.

--- main.pl
+++ main.pl
@@ -1,6 +1,6 @@
-
-% -------------------------TDA Paradigmadocs----------------------------------
+% -------------------------TDA ParadigmaDocs----------------------------------
 % ---------------------------REPRESENTACION-----------------------------------
+
 % El TDA Paradigmadocs se representa a traves de una lista 
 % (string X fecha X list X list X list) la cual contiene un nombre, una fecha y
 % tres listas inicialmente vacias donde seran guardados los usuarios registrados, 
@@ -8,16 +8,35 @@
 
 % ---------------------------------CONSTRUCTOR-------------------------------
 
-% Dominio: Un nombre de tipo string y una fecha
-% Recorrido: Una lista
-
-paradigmadocs(Nombre,Fecha,[Nombre,Fecha,[],[],[]]).
+% Dominio: Un nombre de tipo string, una fecha y un TDA ParadigmaDocs
+% Descripcion: Predicado que crea una plataforma de tipo paradigmaDocs
+paradigmaDocs(Nombre,Fecha,[Nombre,Fecha,[],[],[]]).
 
 % ---------------------------------SELECTORES---------------------------------
 
-getNombre([Nombre|_],Nombre).
-getDate([_,Date,_,_,_] Date).
+% Dominio: Un paradigmaDocs y un nombre de tipo string
+% Descripcion: Predicado que obtiene el nombre de una plataforma de tipo ParadigmaDocs
+getNombrePdocs([Nombre|_],Nombre).
 
+% Dominio: Un paradigmaDocs y una fecha de tipo date
+% Descripcion: Predicado que obtiene la fecha de creacion de una plataforma de tipo ParadigmaDocs
+
+getDatePdocs([_,Date,_,_,_], Date).
+
+% Dominio: Un paradigmaDocs y una lista de usuarios registrados
+% Descripcion: Predicado que obtiene la lista de usuarios registrados de una plataforma de tipo ParadigmaDocs
+getRegistradosPdocs([_,_,Registrados,_,_],Registrados).
+
+% Dominio: Un paradigmaDocs y una lista de users activos
+% Descripcion: Predicado que obtiene la lista de User Activo de una plataforma de tipo ParadigmaDocs
+getActivosPdocs([_,_,_,Activos,_], Activos).
+
+% Dominio: Un paradigmaDocs y una lista de documentos
+% Descripcion: Predicado que obtiene la lista de documentos de una plataforma de tipo ParadigmaDocs
+getDocumentosPdocs([_,_,_,_,Docs], Docs).
+
+paradigmadocsActualizado(Nombre,Fecha,ListRegistrados,ListActivo,ListDocumento,PDOUT):-
+	PDOUT = [Nombre, Fecha, ListRegistrados,ListActivo,ListDocumento].
 % ----------------------------- TDA USER-------------------------------------
 % ---------------------------REPRESENTACION-----------------------------------
 % ------------------------------CONSTRUCTOR----------------------------------
@@ -48,7 +67,7 @@ mes(10,31).
 mes(11,30).
 mes(12,31).
 
-fecha(DD,MM,AAAA,[DD,MM,AAAA]) :-
+date(DD,MM,AAAA,[DD,MM,AAAA]) :-
 	integer(DD), integer(MM), integer(AAAA),
 	mes(MM,Dias),
 	DD>0,
@@ -62,37 +81,28 @@ fecha(DD,MM,AAAA,[DD,MM,AAAA]) :-
 % Paradigmadocs
 % TDA Documento
 
-pertenece(NombreU,[[NombreU,_,_,_,_]|_]):- !.
-pertenece(NombreU,[[_,_,_,_,_]|L]):-
+pertenece(NombreU,[[_,NombreU,_]|_]):- !.
+pertenece(NombreU,[[_,_,_]|L]):-
             pertenece(NombreU,L).
 
 
+% Dominio: Fecha, string, string, TDA Paradigmadocs
+paradigmaDocsRegister(Sn1, Fecha, Username, Password, Sn2):-
+	string(Username),
+	string(Password),
+	getNombrePdocs(Sn1, NamePdocs),
+	getDatePdocs(Sn1,DatePdocs),
+	getRegistradosPdocs(Sn1,Users),
+	getActivosPdocs(Sn1,Activo),
+	getDocumentosPdocs(Sn1,Documents),
+	not(pertenece(Username, Users)),
+	usuario(Fecha,Username,Password,New),
+	append(Users,[New],NewRegistrados),
+	paradigmadocsActualizado(NamePdocs, DatePdocs, NewRegistrados, Activo, Documents, Sn2).
 
 
+% date(20, 12, 2015, D1), paradigmaDocs("google docs", D1, PD1), paradigmaDocsRegister(PD1, D1, "vflores", "hola123", PD2).
 
+% ["google docs", [20, 12, 2015], [[[20, 12, 2015], "vflores", "hola123"]], [], []]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% date(20, 12, 2015, D1), paradigmaDocs("google docs", D1, PD1), paradigmaDocsRegister(PD1, D1, "vflores", "hola123", ["google docs", [20, 12, 2015], [[[20, 12, 2015], "vflores", "hola123"]], [], []]).
