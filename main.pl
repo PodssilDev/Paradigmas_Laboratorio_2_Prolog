5588b300225fd15f0142ecf432a5f55051b0f26a
John <john.serrano@usach.cl>
2021-12-18 19:46:21 -0300

    Se crea TDA Documento, TDA Historial y paradigmaDocsCreate
    Se crea el TDA Documento, TDA Historial y se completan algunos aspectos de documentación. Se crea el predicado paradigmaDocsCreate.

--- main.pl
+++ main.pl
@@ -1,4 +1,6 @@
-% ----------------------------- TDA FECHA-------------------------------------
+% -------------------------LABORATORIO 2: PROLOG-----------------------------
+
+% ----------------------------- TDA DATE-------------------------------------
 % Dominio:
 % cantidad_dias: Entero
 % DD: Entero
@@ -16,7 +18,7 @@
 % El TDA Fecha se representa a traves de una lista (integer X integer X
 % integer), la cual contiene un dia, un mes y un anio.
 
-% ---------------------------------CONSTRUCTOR Y PERTENENCIA------------------
+% --------------------------CONSTRUCTOR Y PERTENENCIA-------------------------
 
 % Clausulas
 % Hechos: 
@@ -61,6 +63,180 @@ getMesFecha([DD,MM,AAAA],MM) :-
 getAnioFecha([DD,MM,AAAA],AAAA) :-
 	date(DD,MM,AAAA,[DD,MM,AAAA]).
 
+% ----------------------------- TDA USER-------------------------------------
+
+% Dominio:
+% Date: Date
+% Nombre: String
+% Password: String
+
+% Predicados:
+% user(Date,Nombre,Password,[Date,Nombre,Password])
+% getUserDate([Date,Nombre,Password], Date)
+% getUserName([Date,Nombre,Password], Nombre)
+% getUserPass([Date,Nombre,Password], Password)
+
+% ---------------------------REPRESENTACION-----------------------------------
+
+% El TDA User se representa a traves de una lista (date X string X string X list)
+% la cual contiene una fecha, un username y un password
+
+% ---------------------------CONSTRUCTOR Y PERTENENCIA---------------------
+
+% Clausulas
+% Reglas
+
+% Dominio: Date, String, String y TDA User
+% Descripcion: Predicado que crea un User valido
+user(Date, Nombre, Password, [Date, Nombre, Password]) :-
+	string(Nombre),
+	string(Password),
+	is_list(Date),
+	length(Date,3),
+	getDiaFecha(Date,DD),
+	getMesFecha(Date,MM),
+	getAnioFecha(Date,AAAA),
+	date(DD,MM,AAAA,Date),!.
+
+% ----------------------------SELECTORES---------------------------------
+
+% Dominio: Un User valido y una date
+% Descripcion: Predicado que obtiene la fecha de un User
+getUserDate([Date,Nombre,Password], Date):-
+	user(Date,Nombre,Password,[Date,Nombre,Password]).
+
+% Dominio: Un user valido y un string
+% Descripcion: Predicado que obtiene el username de un User
+getUserName([Date,Nombre,Password], Nombre):-
+	user(Date,Nombre,Password,[Date,Nombre,Password]).
+
+% Dominio: Un user valido y un string
+% Descripcion: Predicado que obtiene el password de un User
+getUserPass([Date,Nombre,Password], Password):-
+	user(Date,Nombre,Password,[Date,Nombre,Password]).
+% ------------------------------OTROS PREDICADOS--------------------------
+
+
+pertenece(NombreU,[[_,NombreU,_]|_]):- !.
+pertenece(NombreU,[[_,_,_]|L]):-
+            pertenece(NombreU,L).
+
+buscarUsuarioPassword([H|_],Usuario,Password):-
+    getUserName(H,Username),
+    getUserPass(H,Contrasena),
+    (Usuario = Username),
+    (Password = Contrasena).
+buscarUsuarioPassword([_|T],Usuario,Password):-
+    buscarUsuarioPassword(T,Usuario,Password).
+
+% -------------------------TDA HISTORIAL----------------------------------
+% Dominio: 
+% Date: Date (Lista de 3 integers)
+% Contenido: String
+% IDVer: Integer
+
+% Predicados:
+% historial(Date, Contenido, IDVer, [Date, Contenido, IDVer])
+
+% ---------------------------REPRESENTACION-----------------------------------
+
+% El TDA Historial se representa a traves de una lista (date X string X integer)
+% que contiene una fecha, un contenido y un ID 
+
+% ---------------------------CONSTRUCTOR Y PERTENENCIA---------------------
+
+% Clausulas
+% Reglas
+
+% Dominio: Date, String, Integer, TDA Historial
+% Descripcion: Predicado que crea una version de un Documento.
+historial(Date,Contenido,IDVer, [Date, Contenido, IDVer]):-
+	string(Contenido),
+	integer(IDVer),
+	is_list(Date),
+	length(Date,3),
+	getDiaFecha(Date,DD),
+	getMesFecha(Date,MM),
+	getAnioFecha(Date,AAAA),
+	date(DD,MM,AAAA,Date),!.
+
+% ----------------------------SELECTORES---------------------------------
+
+
+
+% -------------------------TDA DOCUMENTO-------------------------------------
+
+% Dominio:
+% Autor: String
+% Date: Date
+% Nombre: String
+% Contenido: String
+% IDDoc: Intgeger
+% Permisos: List
+% HistorialList: List
+
+% Predicados:
+% documento(Autor, Date, Nombre, Contenido, IDDoc, DocFinal)
+% getAutorDocumento([Autor,_,_,_,_,_,_],Autor)
+% getDateDocumento([_,Date,_,_,_,_,_], Date)
+% getNameDocumento([_,_,Nombre,_,_,_,_],Nombre)
+% getContenidoDocumento([_,_,_,Contenido,_,_,_],Contenido)
+% getPermisosDocumento([_,_,_,_,Permisos,_,_],Permisos)
+% getHistorialDocumento([_,_,_,_,_,HistorialList,_],HistorialList)
+% getIDDocumento([_,_,_,_,_,_,IDDoc],IDDoc)
+
+% ---------------------------REPRESENTACION-----------------------------------
+
+% El TDA Documento se representa a traves de una lista (string X date X string
+% X string X list X list X integer) que contiene un Autor, una fecha, un nombre,
+% un contenido (texto), dos listas, donde la primera es la lista de permisos y la
+% segunda es la lista de historial y un ID unico.
+
+% ---------------------------CONSTRUCTOR Y PERTENENCIA---------------------
+%
+documento(Autor,Date,Nombre,Contenido, IDDoc, DocFinal):-
+	string(Autor),
+	string(Nombre),
+	string(Contenido),
+	integer(IDDoc),
+	is_list(Date),
+	length(Date,3),
+	getDiaFecha(Date,DD),
+	getMesFecha(Date,MM),
+	getAnioFecha(Date,AAAA),
+	date(DD,MM,AAAA,Date),!,
+	historial(Date,Contenido,0, Hist1),
+	DocFinal = [Autor,Date,Nombre,Contenido,[],[Hist1],IDDoc].
+
+% ----------------------------SELECTORES---------------------------------
+
+getAutorDocumento([Autor,_,_,_,_,_,_],Autor):-
+	string(Autor).
+
+getDateDocumento([_,Date,_,_,_,_,_], Date):-
+	is_list(Date),
+	length(Date,3),
+	getDiaFecha(Date,DD),
+	getMesFecha(Date,MM),
+	getAnioFecha(Date,AAAA),
+	date(DD,MM,AAAA,Date),!.
+
+getNameDocumento([_,_,Nombre,_,_,_,_],Nombre):-
+	string(Nombre).
+
+getContenidoDocumento([_,_,_,Contenido,_,_,_],Contenido):-
+	string(Contenido).
+
+getPermisosDocumento([_,_,_,_,Permisos,_,_],Permisos):-
+	is_list(Permisos).
+
+getHistorialDocumento([_,_,_,_,_,HistorialList,_],HistorialList):-
+	is_list(HistorialList).
+
+getIDDocumento([_,_,_,_,_,_,IDDoc],IDDoc):-
+	integer(IDDoc).
+
+
 % -------------------------TDA ParadigmaDocs----------------------------------
 % ---------------------------REPRESENTACION-----------------------------------
 
@@ -94,44 +270,17 @@ getRegistradosPdocs([_,_,Registrados,_,_],Registrados).
 % Descripcion: Predicado que obtiene la lista de User Activo de una plataforma de tipo ParadigmaDocs
 getActivosPdocs([_,_,_,Activos,_], Activos).
 
+getUserActivo([UserAct], UserAct).
+
 % Dominio: Un paradigmaDocs y una lista de documentos
 % Descripcion: Predicado que obtiene la lista de documentos de una plataforma de tipo ParadigmaDocs
 getDocumentosPdocs([_,_,_,_,Docs], Docs).
 
 paradigmadocsActualizado(Nombre,Fecha,ListRegistrados,ListActivo,ListDocumento,PDOUT):-
 	PDOUT = [Nombre, Fecha, ListRegistrados,ListActivo,ListDocumento].
-% ----------------------------- TDA USER-------------------------------------
-% ---------------------------REPRESENTACION-----------------------------------
-% ------------------------------CONSTRUCTOR----------------------------------
 
-usuario(Date, Nombre, Password, [Date, Nombre, Password]).
 
-getUserName([Date,Nombre,Password], Nombre):-
-	usuario(Date,Nombre,Password,[Date,Nombre,Password]).
-
-getUserPass([Date,Nombre,Password], Password):-
-	usuario(Date,Nombre,Password,[Date,Nombre,Password]).
-
-% ...
-
-% TDA User
-% TDA Historial
-% TDA Permiso
-% Paradigmadocs
-% TDA Documento
-
-pertenece(NombreU,[[_,NombreU,_]|_]):- !.
-pertenece(NombreU,[[_,_,_]|L]):-
-            pertenece(NombreU,L).
-
-buscarUsuarioPassword([H|_],Usuario,Password):-
-    getUserName(H,Username),
-    getUserPass(H,Contrasena),
-    (Usuario = Username),
-    (Password = Contrasena).
-buscarUsuarioPassword([_|T],Usuario,Password):-
-    buscarUsuarioPassword(T,Usuario,Password).
-
+% -------------------------------PREDICADOS OBLIGATORIOS--------------------------------------------
 
 % Dominio: Fecha, string, string, TDA Paradigmadocs
 paradigmaDocsRegister(Sn1, Fecha, Username, Password, Sn2):-
@@ -143,7 +292,7 @@ paradigmaDocsRegister(Sn1, Fecha, Username, Password, Sn2):-
 	getActivosPdocs(Sn1,Activo),
 	getDocumentosPdocs(Sn1,Documents),
 	not(pertenece(Username, Users)),
-	usuario(Fecha,Username,Password,New),
+	user(Fecha,Username,Password,New),
 	append(Users,[New],NewRegistrados),
 	paradigmadocsActualizado(NamePdocs, DatePdocs, NewRegistrados, Activo, Documents, Sn2).
 
@@ -159,6 +308,24 @@ paradigmaDocsLogin(Sn1,Username,Password,Sn2):-
     getDocumentosPdocs(Sn1,Documents),
     append(Activo,[Username],NewActivo),
     paradigmadocsActualizado(NamePdocs,DatePdocs,Users,NewActivo,Documents, Sn2).
+
+paradigmaDocsCreate(Sn1, Fecha, Nombre, Contenido, Sn2):-
+	string(Nombre),
+	string(Contenido),
+	getActivosPdocs(Sn1,Activo),
+	length(Activo,1),
+	getUserActivo(Activo,UserActivo),
+	getNombrePdocs(Sn1,NamePdocs),
+	getDatePdocs(Sn1,DatePdocs),
+	getRegistradosPdocs(Sn1,Users),
+	getDocumentosPdocs(Sn1,Documents),
+	length(Documents,IDDoc),
+	documento(UserActivo, Fecha, Nombre, Contenido, IDDoc, Doc),
+	append(Documents, [Doc], NewDocuments),
+	paradigmadocsActualizado(NamePdocs, DatePdocs, Users, [], NewDocuments, Sn2).
+
+%paradigmaDocsShare(Sn1, DocumentId, ListaPermisos, ListaUsernamesPermitidos)
+	
 
 % date(20, 12, 2015, D1), paradigmaDocs("google docs", D1, PD1), paradigmaDocsRegister(PD1, D1, "vflores", "hola123", PD2).
 
